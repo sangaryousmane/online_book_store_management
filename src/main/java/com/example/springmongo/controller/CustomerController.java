ff2a71e6ae6649a7da0689e450a45f5760208cb8
ousmane <sangaryousmane@gmail.com>
2023-09-24 19:33:57 +0800

    Update project

---
+++ src/main/java/com/example/springmongo/controller/CustomerController.java
@@ -0,0 +1,2 @@
+package com.example.springmongo.controller;public class CustomerController {
+}
--- src/main/java/com/example/springmongo/security/entities/token/TokenType.java
+++
@@ -1,9 +0,0 @@
-package com.example.springmongo.security.entities.token;
-
-public enum TokenType {
-
-    /**
-     * A Bearer token for JWT authentication
-     */
-    BEARER
-}
